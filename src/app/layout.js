04b1be88ebdc96de4eebd3716b5b3a57e908b06f
sitthisak phisanphiphak <sitthisak2007.1@gmail.com>
2025-08-18 10:20:30 +0700

    fix font

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="th">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body>
         {children}
